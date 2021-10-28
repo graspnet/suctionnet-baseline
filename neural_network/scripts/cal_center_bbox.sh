f7d19b8468db9467431b4bc7ae5630b9218db7c1
intrepidChw <mbd_chw@sjtu.edu.cn>
2021-10-28 15:17:11 -0700

    add scripts of score_mapping and cal_center_bbox

---
+++ neural_network/scripts/cal_center_bbox.sh
@@ -0,0 +1,5 @@
+python cal_center_bbox.py --dataset_root /DATA2/Benchmark/graspnet \
+--saveroot /DATA2/Benchmark/suction/additional_label \
+--camera realsense \
+--pool_size 10 \
+--save_visu
---
+++ neural_network/scripts/score_mapping.sh
@@ -0,0 +1,6 @@
+python score_mapping.py --dataset_root /DATA2/Benchmark/graspnet \
+--saveroot /DATA2/Benchmark/suction/additional_label \
+--camera realsense \
+--sigma 4 \
+--pool_size 10 \
+--save_visu
